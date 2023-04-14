d1c63cdbdfa6258878d3eb99350da9932bb965cc
Conrad Nunes <nunes.conrad93@gmail.com>
2023-04-14 19:29:37 +0530

    data set

--- server/services/bot.js
+++ server/services/bot.js
@@ -4,24 +4,38 @@ const fs = require("fs");
 const path = require("path");
 
 const train = async () => {
-    const files = fs.readdirSync(path.join(__dirname, "..", "intents"));
-    for (const file of files) {
-        let data = fs.readFileSync(path.join(__dirname, "..", `intents/${file}`));
-        data = JSON.parse(data);
-        let intent = file.replace(".json","");
-        for (const question of data.questions) {
-            manager.addDocument("en", question, intent);
+    //old
+    // const files = fs.readdirSync(path.join(__dirname, "..", "intents"));
+    // for (const file of files) {
+    //     let data = fs.readFileSync(path.join(__dirname, "..", `intents/${file}`));
+    //     data = JSON.parse(data);
+    //     let intent = file.replace(".json","");
+    //     for (const question of data.questions) {
+    //         manager.addDocument("en", question, intent);
+    //     }
+    //     for (const answer of data.answers) {
+    //         manager.addAnswer("en", intent, answer);
+    //     }
+    // }
+
+    //new
+    let data = fs.readFileSync(path.join(__dirname, "..", `intents/data.json`));
+    data = JSON.parse(data);
+    data.intents.forEach(element => {
+        for (const pattern of element.patterns) {
+            manager.addDocument("en", pattern, element.tag);
         }
-        for (const answer of data.answers) {
-            manager.addAnswer("en", intent, answer);
+        for (const response of element.responses) {
+            manager.addAnswer("en", element.tag, response);
         }
-    }
+    });
+
     await manager.train();
     manager.save();
+    manager.load();
 };
 
 const post = async (data) => {
-    manager.load();
     let response = await manager.process("en", data);
     return response;
 };
